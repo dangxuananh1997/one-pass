fa5f46d7b4be41f80826cbd06a5a3ff64f9171f4
Dang Xuan Anh <dangxuananh1997@users.noreply.github.com>
2018-05-28 23:22:51 +0700

    color theme + action sheets

--- src/components/site/site.ts
+++ src/components/site/site.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Site } from '../../models/site';
+import { ActionSheetController } from 'ionic-angular';
 
 @Component({
   selector: 'site',
@@ -10,14 +11,45 @@ export class SiteComponent implements OnInit {
 
   showPassword: boolean = false;
 
-  constructor() {
+  constructor(public actionSheetCtrl: ActionSheetController) {
   }
 
   ngOnInit() {
   }
 
-  showSiteOptions() {
-    console.log('clicked');
+  showSiteOptions(site: Site) {
+    let actionSheet = this.actionSheetCtrl.create({
+      buttons: [
+        {
+          text: 'Show details',
+          icon: 'ios-book-outline',
+          cssClass: 'tests',
+          handler: () => { }
+        }, 
+        {
+          text: 'Copy username',
+          icon: 'ios-copy-outline',
+          handler: () => { }
+        }, 
+        {
+          text: 'Copy password',
+          icon: 'ios-copy',
+          handler: () => { }
+        }, 
+        {
+          text: 'Edit',
+          icon: 'ios-create-outline',
+          handler: () => { }
+        }, 
+        {
+          text: 'Delete',
+          cssClass: 'danger',
+          icon: 'ios-trash-outline',
+          handler: () => { }
+        }
+      ]
+    });
+    actionSheet.present();
     
   }
 
