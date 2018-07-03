6b1f97bd6820aba7805a8fe90480705bb7fdaffc
Dang Xuan Anh <dangxuananh1997@users.noreply.github.com>
2018-07-04 00:25:21 +0700

    ...

--- src/pages/generate-password/generate-password.ts
+++ src/pages/generate-password/generate-password.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
 @Component({
   selector: 'page-generate-password',
   templateUrl: 'generate-password.html',
 })
-export class GeneratePasswordPage {
+export class GeneratePasswordPage implements OnInit {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  characterCount: number = 6;
+  password: string = '';
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) { }
+
+  ngOnInit() {
+    this.generatePassword();
+  }
+
+  generatePassword() {
+    this.password = '';
+    for (let i = 0; i < this.characterCount; i++) {
+      this.password += String.fromCharCode(Math.floor(Math.random() * 93) + 33);
+    }
   }
 
 }
